0cc142f2b8adc3c75648a73576a97509454b5354
SediqaAhmadi <sediqa.ahmadi@gmail.com>
2022-07-25 23:59:42 -0400

    This is the 1st GitHub Practice

---
+++ src/first/exampl/Main.java
@@ -0,0 +1,9 @@
+package first.exampl;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("This is my 1st Assignment");
+		System.out.println(" This is the copy of the copy of the Assignment");
+	}
+
+}
